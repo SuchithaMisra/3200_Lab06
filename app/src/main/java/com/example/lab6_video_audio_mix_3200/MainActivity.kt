c8f4e0063201ab9f1ca6ead89fa8bf2007fc7b24
SuchithaMisra <suchitha.misra@uconn.edu>
2024-04-18 21:41:09 -0400

    clean up

--- app/src/main/java/com/example/lab6_video_audio_mix_3200/MainActivity.kt
+++ app/src/main/java/com/example/lab6_video_audio_mix_3200/MainActivity.kt
@@ -4,13 +4,7 @@ import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -18,12 +12,12 @@ import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.SavedStateHandle
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.ui.PlayerView
 import com.example.lab6_video_audio_mix_3200.viewModels.MainViewModel
@@ -62,17 +56,17 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                 ) {
                     Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                        // Display the video player
+                        // Display the main video player
                         AndroidView({ context -> PlayerView(context).apply { player = mainViewModel.videoPlayer as ExoPlayer } },
                             modifier = Modifier.height(200.dp).fillMaxWidth())
 
-                        // Display the audio player
-                        AndroidView({ context -> PlayerView(context).apply { player = mainViewModel.audioPlayer as ExoPlayer } },
-                            modifier = Modifier.height(200.dp).fillMaxWidth())
-
-                        LazyColumn(modifier = Modifier.weight(1f).padding(vertical = 8.dp)) { // Adjusted padding here
+                        // Buttons to control main video playback
+                        LazyColumn(modifier = Modifier.padding(vertical = 8.dp)) {
                             items(mainViewModel.videoUrisValue) { (videoUri, name) ->
-                                Row(horizontalArrangement = Arrangement.SpaceEvenly) {
+                                Row(
+                                    horizontalArrangement = Arrangement.Center,
+                                    modifier = Modifier.fillMaxWidth()
+                                ) {
                                     Button(onClick = {
                                         if (mainViewModel.videoPlayer.isPlaying) {
                                             mainViewModel.pauseVideo()
@@ -86,9 +80,13 @@ class MainActivity : ComponentActivity() {
                             }
                         }
 
-                        LazyColumn(modifier = Modifier.weight(1f).padding(vertical = 8.dp)) { // Adjust padding
+                        // Buttons to control audio playback
+                        LazyColumn(modifier = Modifier.padding(vertical = 8.dp)) {
                             items(mainViewModel.audioUrisValue) { (audioUri, name) ->
-                                Row(horizontalArrangement = Arrangement.SpaceEvenly) {
+                                Row(
+                                    horizontalArrangement = Arrangement.Center,
+                                    modifier = Modifier.fillMaxWidth()
+                                ) {
                                     Button(onClick = {
                                         if (mainViewModel.audioPlayer.isPlaying) {
                                             mainViewModel.pauseAudio()
